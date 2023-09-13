b23509136a7fd898d8e497de2fd4c7205d5d57b1
Ahsan Ahmed Rakib <ahsan.rakib@technonext.com>
2023-09-13 17:28:36 +0600

    Fetch data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-import Counter from "./components/Counter";
+// import Counter from "./components/Counter";
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to React Redux</h1>
-      <Counter />
+      <h1>Welcome to React Redux - Todos</h1>
+      {/* <Counter /> */}
+      <Todos />
     </div>
   );
 }
--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
-import { createStore } from "redux";
-import counterReducer from "./services/reducers/counterReducer";
+import { applyMiddleware, createStore } from "redux";
+// import counterReducer from "./services/reducers/counterReducer";
+import todosReducer from "./services/reducers/todosReducer";
+const thunk = require("redux-thunk").default;
 
-const store = createStore(counterReducer);
+// const store = createStore(counterReducer);
+const store = createStore(todosReducer, applyMiddleware(thunk));
 export default store;
